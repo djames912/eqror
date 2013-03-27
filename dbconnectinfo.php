5fbeb2d224baa5c0677c157be97f09dc5bfc53a1
djames912 <dwj@lowfatmail.com>
2013-03-26 21:09:53 -0600

    Reverted change to dbconnect() since it broke testing pages and functions.

---
+++ dbconnectinfo.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$dbinfo = array();
+$dbInfo['server'] = "localhost";
+$dbInfo['database'] = "orgror";
+$dbInfo['username'] = "roruser";
+$dbInfo['password'] = "ClEAnm3Up!";
+
+?>
