aa9989c23b52ce44889ccf7fcd09590d90dd526c
djames912 <dwj@lowfatmail.com>
2013-04-27 16:29:56 -0600

    Added some extra fields to the member portion of the admin page.  This is an experiment more than anything.

--- pages/admin.php
+++ pages/admin.php
@@ -10,7 +10,17 @@
     <span id="positions"></span>
 </div>
 <div class="grid_8">
-    <input type="text" id="member" name="member"> <button id="submitmem" onClick="addMember(); ">Add Member</button>
+    Surname: <input type="text" id="mbrsur" name="mbrsur">
+    <br>
+    Given Name: <input type="text" id="mbrgivn" name="mbrgivn">
+    <br>
+    Middle: <input type="text" id="mbrmidl" name="mbrmidl">
+    <br>
+    Suffix: <input type="text" id="mbrsufx" name="mbrsufx">
+    <br>
+    Prefers: <input type="text" id="mbrpref" name="mbrpref">
+    <br>
+    <button id="submitmem" onClick="addMember(); ">Add Member</button>
     <h1>Members in the database</h1>
     <span id="members"></span>
 </div>
